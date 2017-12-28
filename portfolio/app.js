5a1979168cafb83e526e715cfa74624fd2e2798a
cuo <cuo2017@163.com>
2017-12-28 14:20:52 +0800

    123

---
+++ portfolio/app.js
@@ -0,0 +1,12 @@
+$('#b1').click(function(event) {
+	/* Act on the event */
+	$('html,body').animate({scrollTop: '0px'}, 300);
+});
+$('#b2').click(function(event) {
+	/* Act on the event */
+	$('html,body').animate({scrollTop: '400px'}, 300);
+});
+$('#b3').click(function(event) {
+	/* Act on the event */
+	$('html,body').animate({scrollTop: '1150px'}, 300);
+});
